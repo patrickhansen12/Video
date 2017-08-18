1597be32e1dee4f041f483ae8fe3920965c23b1c
Patrick <mussi_kat@hotmail.com>
2017-08-18 11:39:36 +0200

    Everything works the way It should
    -Removed the second arraylist as it wasnt being used anymore
    +added add/delete/edit and list overview

--- videoMenu/Program.cs
+++ videoMenu/Program.cs
@@ -1,20 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
 
 namespace videoMenu
 {
     class Program
     {
+        private static int id = 1;
+        static List<Videoes> videoes = new List<Videoes>();
         public static void Main(string[] args)
         {
-            Console.WriteLine("select what you want to do");
-            Console.WriteLine("_______________________________");
+          
+          
+             Videoes vid = new Videoes()
+            {
+                VideoName = "Fish talks:",
+                VideoLenght = " Videolenght 5:0 minutes:" + " Video id: ",
+                VideoId = id++
+            };
+            videoes.Add(vid);
+            videoes.Add(new Videoes()
+            {
+                VideoName = "Bob goes to the store:",
+                VideoLenght = " Videolenght 5:0 minutes:" + " Video id: ",
+                VideoId = id++
+            });
+            
+
             string[] menuItems =
             {
 
                 "List all the videoes",
                 "Add new video",
-                "Video options",
+                "Delete a video",
+                "Edit a video",
                 "Exit"
 
             };
@@ -22,50 +41,44 @@ namespace videoMenu
            
     
             var selection = ShowMenu(menuItems);
-            while (selection !=4)
+            while (selection !=5)
             {
 
                 switch (selection)
                 {
+
                     case 1:
+                        Console.WriteLine("");
                         Console.WriteLine("List of videoes");
+                       listvideo();
+                        Console.WriteLine("");
+                     
                        
-                        string[] vidoes =
-                        {
 
-                            "Cats playing with dogs",
-                            "fish drowning",
-                            "Cow barking",
-                            "Exit"
-                           
-                        };
-                        for (int i = 0; i < menuItems.Length; i++)
-                        {
-                            Console.WriteLine((i + 1) + ": " + vidoes[i]);
-
-
-                        }
-                        int exit;
-                        
-                        exit = int.Parse(Console.ReadLine());
-                        if (exit == 4)
-                        {
-                            System.Environment.Exit(4);
-                        }
                         break;
                     case 2:
                         Console.WriteLine("add a new video");
+                        AddVideos();
+                     
                         break;
                     case 3:
-                        Console.WriteLine("Video options");
+                        Console.WriteLine("Delete Video");
+                        DeleteVideos();
                         break;
-                    case 4:
+                        case 4:
+                            
+                        EditVideo();
+                        break;
+                    case 5:
                         Console.WriteLine("Exit");
-                        System.Environment.Exit(4);
+                       
                         break;
                 }
-                //selection = ShowMenu(menuItems);
+                selection = ShowMenu(menuItems);
+                
+                
             }
+          
             Console.WriteLine("bye world");
 
             
@@ -75,8 +88,8 @@ namespace videoMenu
         {
 
                 
-            //Console.WriteLine("select what you want to do");
-            //Console.WriteLine("_______________________________");
+            Console.WriteLine("select what you want to do");
+            Console.WriteLine("_______________________________");
 
             for (int i = 0; i < menuItems.Length; i++)
             {
@@ -87,10 +100,10 @@ namespace videoMenu
 
             }
             int selection;
-            while (!int.TryParse(Console.ReadLine(), out selection) || selection < 0 || selection > 4)
+            while (!int.TryParse(Console.ReadLine(), out selection) || selection < 0 || selection > 5)
             {
              
-                Console.WriteLine("you need to select a number between 1-4");
+                Console.WriteLine("you need to select a number between 1-5");
               
             }
 
@@ -99,8 +112,60 @@ namespace videoMenu
             return selection;
 
         }
-       
-    
+        private static Videoes FindVideoById()
+        {
+            Console.WriteLine("Insert Video Id: ");
+            int id;
+            while (!int.TryParse(Console.ReadLine(), out id))
+            {
+                Console.WriteLine("Please insert a number");
+            }
+
+            foreach (var Videoes in videoes)
+            {
+                if (Videoes.VideoId == id)
+                {
+                    return Videoes;
+                }
+            }
+            return null;
+        }
+        private static void DeleteVideos()
+        {
+            var videoFound = FindVideoById();
+            if (videoFound != null)
+            {
+                videoes.Remove(videoFound);
+            }
+        }
+
+        public static void EditVideo()
+        {
+            var videoes = FindVideoById();
+            Console.WriteLine("name");
+            videoes.VideoName = Console.ReadLine();
+        }
+        private static void AddVideos()
+        {
+            Console.WriteLine("Name:  ");
+            var name = Console.ReadLine();
+
+            videoes.Add(new Videoes()
+            {
+                VideoName = name,
+                VideoId = id++
+            });
+        }
+
+
+        public static void listvideo()
+        {
+            foreach (var vidoes in videoes)
+            {
+                Console.WriteLine($"Name : {vidoes.VideoName}" + $"{vidoes.VideoLenght}" + $"{vidoes.VideoId}");
+            }
+        
+        }
 
 }
 }
---
+++ videoMenu/Videoes.cs
@@ -0,0 +1,10 @@
+﻿namespace videoMenu
+{
+    public class Videoes
+    {
+        public string VideoName { get; set; }
+        public string VideoLenght { get; set; }
+        public int VideoId { get; set; }
+
+    }
+}
